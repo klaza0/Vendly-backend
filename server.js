8ec05a2e4bba06e950a3410bdd6bcd6f4c4a82ae
Ibrahim Montaser <imontaser6@example.com>
2026-01-04 17:21:15 +0200

    تحديث المشروع: اصلاح البورت وتشغيل على Render

--- server.js
+++ server.js
@@ -8,15 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoURI = process.env.MONGO_URI;
-mongoose.connect(mongoURI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => {
-    console.error("MongoDB connection error:", err);
-    process.exit(1);
-});
-
-// Routes
 const authRoutes = require("./src/routes/auth.routes");
 const userRoutes = require("./src/routes/user.routes");
 const productRoutes = require("./src/routes/product.routes");
@@ -27,7 +18,17 @@ app.use("/users", userRoutes);
 app.use("/products", productRoutes);
 app.use("/sales", saleRoutes);
 
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI)
+.then(() => {
+  console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.error("MongoDB connection error:", err);
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
