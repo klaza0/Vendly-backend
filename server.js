4bb9e347d557415c40afde94594844b6c59360f8
Ibrahim Montaser <imontaser6@example.com>
2026-01-04 17:17:50 +0200

    Update full project with JWT, Roles, Subscription, fixes

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use("/users", userRoutes);
 app.use("/products", productRoutes);
 app.use("/sales", saleRoutes);
 
-// Start server
-const PORT = process.env.PORT || 5000; // Render يعطي الـ PORT تلقائيًا
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
